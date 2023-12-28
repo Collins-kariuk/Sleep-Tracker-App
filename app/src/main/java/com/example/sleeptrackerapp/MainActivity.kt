25c14ea7c3143ea4d5d5e1abc03505c0036145c5
Collins Kariuki <cmka2020@mymail.pomona.edu>
2023-12-28 10:29:50 -0800

    Add initial screen text at home page

--- app/src/main/java/com/example/sleeptrackerapp/MainActivity.kt
+++ app/src/main/java/com/example/sleeptrackerapp/MainActivity.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -12,7 +16,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.sleeptrackerapp.ui.theme.SleepTrackerAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,9 +34,41 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     BackgroundImage()
+                    InitialScreenText(
+                        welcome = "Welcome to Better Sleep",
+                        ready = "Are you ready to better your sleep?",
+                        modifier = Modifier
+                    )
                 }
             }
         }
+    }
+}
+
+@Composable
+fun InitialScreenText(welcome: String, ready: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(top = 16.dp), // Add padding at the top
+        verticalArrangement = Arrangement.Top // Align the content to the top
+    ) {
+        Text(
+            text = welcome,
+            fontSize = 24.sp, // Reduced font size
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth() // Ensure the text is centered
+                .padding(16.dp)
+        )
+        Text(
+            text = ready,
+            fontSize = 18.sp, // Reduced font size
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth() // Ensure the text is centered
+                .padding(16.dp)
+        )
     }
 }
 
@@ -37,7 +77,7 @@ fun BackgroundImage(modifier: Modifier = Modifier) {
     val image = painterResource(id = R.drawable.blumenwiese_bei_obermaiselstein)
     Image (
         painter = image,
-        contentDescription = "A meadow to serve as a background for the splash screen",
+        contentDescription = stringResource(R.string.background_description),
         contentScale = ContentScale.Crop,
         alpha = 0.7f
     )
@@ -45,8 +85,14 @@ fun BackgroundImage(modifier: Modifier = Modifier) {
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun SleepTrackerPreview() {
     SleepTrackerAppTheme {
-        BackgroundImage()
+        BackgroundImage(modifier = Modifier.fillMaxSize())
+
+        InitialScreenText(
+            welcome = "Welcome to Better Sleep",
+            ready = "Are you ready to better your sleep?",
+            modifier = Modifier
+        )
     }
 }
